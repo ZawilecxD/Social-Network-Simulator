a05b54c7f4c1cc6fe4a5a49ce9afd90eec53927c
ZawilecxD <zawilecxd@gmail.com>
2016-04-10 20:27:38 +0200

    new classes

---
+++ Social Network/src/network/Event.java
@@ -0,0 +1,10 @@
+package network;
+
+import java.util.List;
+
+public class Event {
+
+	public String name;
+	public List<User> users;
+	
+}
---
+++ Social Network/src/network/Group.java
@@ -0,0 +1,10 @@
+package network;
+
+import java.util.List;
+
+public class Group {
+	
+	public String name;
+	public List<User> users; 
+
+}
